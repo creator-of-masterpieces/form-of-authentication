ffafa0572c318dac0fd4d854ec98830df4f0c6aa
Dmitrii Timonov <timonov.dim13@gmail.com>
2023-04-06 05:10:40 +0300

    Создал и стилизовал форму
    Создал форму и сделал базовую стилизацию.

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from fastapi.responses import Response
 
+#Для запуска сервера введите в консоль команду uvicorn server:app --reload
+
 #Создаём экземляр приложения
 app = FastAPI()
 
-#При запросе к адресу / Вернёт страницу с текстом "Привет!"
 @app.get("/")
 def index_page():
-    return Response("Привет!", media_type="text/html")
+    """При запросе к адресу / Вернёт страницу с текстом 'Привет!'"""
+    with open('templates/login.html', 'r') as f:
+        login_page = f.read()
+        return Response(login_page, media_type="text/html")
